e2c730d9ed0fe9a31a231c90cbbb6a758fa32e4f
mmohammadi83 <mm4555173@gmail.com>
2025-01-03 17:36:30 +0330

    sakht file hash map

--- hashMap.h
+++ hashMap.h
@@ -3,6 +3,64 @@
 
 class Map{
 private:
-    
+    class Entry{
+    public:
+        std::string key;
+        std::string value; 
+        bool isFull;
 
+        Entry():key("") , value("") , isFull(false){}
+    };
+    int capacity;
+    Entry* array;
+
+    int hash(std::string key){
+        int index = 0;
+        for(auto& a: key){
+            index += a;
+        }
+        index = index % capacity;
+
+        return index;
+    }
+
+public:
+    Map(int c): capacity(c){
+        array = new Entry[c];
+    }
+
+    void insert(std::string key , std::string value){
+        int i = hash(key);
+        while(array[i].isFull){
+            if(array[i].key == key){
+                array[i].value = value;
+                return;
+            }
+            i = (i+1) % capacity;
+        }
+        array[i].key = key;
+        array[i].value = value;
+        array[i].isFull = true;
+
+    }
+
+    int exists(std::string key){
+        for(int i=0 ; i<capacity ; i++){
+            if(array[i].isFull && array[i].key == key){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    std::string get(std::string key){
+        if(exists(key) != -1){
+            return array[exists(key)].value;
+        }
+        return "Empty";
+    }
+    ~Map(){
+        delete[] array;
+        array = nullptr;
+    }
 };
